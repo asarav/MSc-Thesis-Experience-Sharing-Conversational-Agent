f8128abc7f60a46504be9474cc57c5e1d4e12620
Avinash Saravanan <asarav@umich.edu>
2020-12-31 16:47:31 -0500

    Finish work with information retrieval based flows

--- Client/dialogue_states/greeting.py
+++ Client/dialogue_states/greeting.py
@@ -1,11 +1,14 @@
 import management_utils.response_manager as ResponseManager
+import management_utils.search_based_conversation as SBC
 
 class Greeting:
     def __init__(self):
         self.responseUtils = ResponseManager.ResponseManager()
+        self.DiabetesAnswers = SBC.SearchBasedConversation([], "Diabetes Questions")
         self.ID = "1234"
         self.username = ""
         self.IncorrectName = False
+        self.firstTimeDiabetesQuestion = True
         self.states = [{
             "name": "AskUserID",
             "statement": "Hello there. I am Diet Bot. Before we start, do you already have a user id?",
@@ -70,7 +73,49 @@ class Greeting:
         {
             "name": "GetStartedGreeting",
             "statement": self.GetStartedGreetingStatement,
-            "response": "GetStartedGreeting",
+            "response": "IntroduceProcesses",
+            "stateType": "Statement"
+        },
+        {
+            "name": "IntroduceProcesses",
+            "statement": "To begin, I will be working with you to develop a positive diet related habit over the next three days that can help you manage or prevent type II diabetes more effectively.",
+            "response": "ExplainTypeIIDiabetes",
+            "stateType": "Statement"
+        },
+        {
+            "name": "ExplainTypeIIDiabetes",
+            "statement": "Type 2 diabetes is a condition that results in a high blood glucose level. Blood glucose is also known as blood sugar. Type 2 diabetes results in symptoms like increased thirst and tiredness. Long term effects can be more serious. Long term effects include, but are not limited to heart disease, strokes, and kidney failure. Needless to say, the effects of diabetes when left untreated are extremely serious.",
+            "response": "ExplainTreatments",
+            "stateType": "Statement"
+        },
+        {
+            "name": "ExplainTreatments",
+            "statement": "There are a variety of treatments for Type II diabetes, but two approaches that are under your control are that of diet management and exercise. I will be focusing on diet.",
+            "response": "AnswerDiabetesQuestions",
+            "stateType": "Statement"
+        },
+        {
+            "name": "AnswerDiabetesQuestions",
+            "statement": self.AnswerDiabetesQuestionsStatement,
+            "response": self.AnswerDiabetesQuestionsResponse,
+            "stateType": "AnswerResponse"
+        },
+        {
+            "name": "AskDiabetesQuestion",
+            "statement": "What is your question?",
+            "response": self.AskDiabetesQuestionResponse,
+            "stateType": "AnswerResponse"
+        },
+        {
+            "name": "DiabetesAnswer",
+            "statement": self.ProvideDiabetesAnswer,
+            "response": "AnswerDiabetesQuestions",
+            "stateType": "Statement"
+        },
+        {
+            "name": "ListGoals",
+            "statement": "There are three goals that you can choose. These are calorie restriction, sugar reduction and diet composition change. Before we choose a goal, I would like to ask you for a few personal details so that we can ensure that the goal that is chosen is appropriate for you.",
+            "response": "ListGoals",
             "stateType": "Statement"
         }
         ]
@@ -136,3 +181,27 @@ class Greeting:
 
     def GetStartedGreetingStatement(self):
         return "Great. Nice to meet you " + self.username + ". Let's start improving your diet"
+
+    def AnswerDiabetesQuestionsResponse(self, response):
+        nextState = "AskDiabetesQuestion"
+        decision = self.responseUtils.YesOrNo(response)
+        if decision is 0:
+            nextState = "ListGoals"
+        else:
+            nextState = "AskDiabetesQuestion"
+        return [], nextState
+
+    def AnswerDiabetesQuestionsStatement(self):
+        if not self.firstTimeDiabetesQuestion:
+            return "Do you have any other questions?"
+        else:
+            self.firstTimeDiabetesQuestion = False
+            return "Do you have any questions about Type 2 Diabetes So far?"
+
+    def AskDiabetesQuestionResponse(self, response):
+        nextState = "DiabetesAnswer"
+        self.DiabetesQuestionAnswer = self.DiabetesAnswers.askQuestion(response)
+        return [], nextState
+
+    def ProvideDiabetesAnswer(self):
+        return self.DiabetesQuestionAnswer
--- Furhat/src/main/kotlin/furhatos/app/dietcoach/flow/dietcoach.kt
+++ Furhat/src/main/kotlin/furhatos/app/dietcoach/flow/dietcoach.kt
@@ -11,7 +11,7 @@ import furhatos.gestures.Gestures
 
 val Start : State = state(Interaction) {
     onEntry {
-        furhat.userSpeechStartGesture = listOf(Gestures.Thoughtful, Gestures.Nod, Gestures.Smile)
+        furhat.userSpeechStartGesture = listOf(Gestures.Thoughtful,Gestures.Smile,Gestures.BrowRaise,Gestures.Nod)
         val statementObject = dialog.getStatement()
         val statmentOutput = statementObject.getString("outputs")
         val gesture = statementObject.getString("gesture")
@@ -172,7 +172,7 @@ fun handleGesture(gest: String) : Gesture {
         "Thoughtful" -> return Gestures.Thoughtful
         "Wink" -> return Gestures.Wink
         else -> { // Note the block
-            return Gestures.Nod
+            return Gestures.BrowRaise
         }
     }
 }
