7ee180c2d0d4d5cf6272f01e77777b217637fba3
Avinash Saravanan <asarav@umich.edu>
2021-01-31 16:57:25 +0100

    Enable saving to long term memory

--- Client/data_retrieval/memoryManager.py
+++ Client/data_retrieval/memoryManager.py
@@ -1,7 +1,7 @@
 import data_retrieval.jsonManager as jsonManager
 
 #A wrapper for the jsonManager that is used for accessing interaction data in different flows.
-class ShortTermData:
+class MemoryManager:
     def __init__(self):
         # READING
         # Opening JSON file
@@ -20,4 +20,13 @@ class ShortTermData:
     #Used to save data before going to another flow
     def writeData(self):
         self.jsonManager.writeJSON(self.data, self.fileName)
+        self.jsonSaved = True
+
+    def readDataFromLongTermMemory(self, id):
+        fileExists = self.jsonManager.readJSON("interaction_data/" + id + ".json")
+        self.data = self.jsonManager.data
+        self.jsonLoaded = True
+
+    def writeDataToLongTermMemory(self, id):
+        self.jsonManager.writeJSON(self.data, "interaction_data/" + id + ".json")
         self.jsonSaved = True
