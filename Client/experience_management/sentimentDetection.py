c9ce2095a35b5b942c1a1c32eb84260aa5277027
Avinash Saravanan <asarav@umich.edu>
2021-03-06 11:18:13 +0100

    Try out decision tree and vader based sentiment detection
    Vader is faster than naive bayes, but less accurate.
    Decision trees are slower than naive bayes, and have similar accuracy.
    Naive bayes is still best after testing with k fold validation.

--- Client/experience_management/sentimentDetection.py
+++ Client/experience_management/sentimentDetection.py
@@ -1,8 +1,11 @@
+import nltk
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import twitter_samples, stopwords
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
 from nltk import FreqDist, classify, NaiveBayesClassifier
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+#nltk.download('vader_lexicon')
 
 import re, string, random
 
@@ -83,3 +86,35 @@ def performSentimentAnalysis(toBeProcessed):
     custom_tokens = remove_noise(word_tokenize(custom_tweet))
 
     return classifier.classify(dict([token, True] for token in custom_tokens))
+
+#Worse accuracy for negative sentiment compared to Naive Bayes. Total accuracy of 0.876
+def performSentimentAnalysisVader(toBeProcessed):
+    positive_tweet_tokens = twitter_samples.strings('positive_tweets.json')
+    negative_tweet_tokens = twitter_samples.strings('negative_tweets.json')
+
+    analyzer = SentimentIntensityAnalyzer()
+
+    #Get accuracy
+    correct = 0
+    incorrect = 0
+    for item in positive_tweet_tokens:
+        if analyzer.polarity_scores(item)["compound"] >= 0:
+            correct = correct + 1
+        else:
+            incorrect = incorrect + 1
+
+    print(correct/(correct + incorrect))
+
+    correct2 = 0
+    incorrect2 = 0
+    for item in negative_tweet_tokens:
+        if analyzer.polarity_scores(item)["compound"] <= 0:
+            correct2 = correct2 + 1
+        else:
+            incorrect2 = incorrect2 + 1
+
+    print(correct2/(correct2 + incorrect2))
+
+    print((correct + correct2) / (correct + correct2 + incorrect + incorrect2))
+
+    return analyzer.polarity_scores(toBeProcessed)
