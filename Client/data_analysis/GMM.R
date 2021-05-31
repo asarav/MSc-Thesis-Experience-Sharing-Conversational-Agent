8ab1e5e4488f34dd178331f91efa59284493ae69
Avinash Saravanan <asarav@umich.edu>
2021-05-31 12:51:40 +0200

    Add gmm r script so that it is available for viewing

--- Client/data_analysis/GMM.R
+++ Client/data_analysis/GMM.R
@@ -1,12 +1,6 @@
 AQ=read.csv("gmmdata.csv")
-library(nlme)
 
-resc=lme(finalGoalAchievementWithoutGoalChange ~ milestoneAchievement+age+motivationBeforeDuringChange+milestoneAdherence+condition+agreementPercentage+futureWork+priorEfficacy+postEfficacy+efficacyChange+education+fakeNatural+machineHuman+consciousUnconscious+artificialLifelike+rigidElegant+deadAlive+stagnantLively+mechanicalOrganic+inertInteractive+apatheticResponsive+dislikeLike+unfriendlyFriendly+unkindKind+unpleasantPleasant+awfulNice+incompetentCompetent+ignorantKnowledgeable+irresponsibleResponsible+unintelligentIntelligent+foolishSensible+anxiousRelaxedBefore+agitatedCalmBefore+quiescentSurprisedBefore+anxiousRelaxedAfter+agitatedCalmAfter+quiescentSurprisedAfter+anxiousRelaxedChange+agitatedCalmChange+quiescentSurprisedChange+typeOfRelationship+usefulOrNotDiabetes+usefulOrNotObesity+convenience+preference+motivationBefore+motivation+motivationAfter+motivationBeforeDuringChange+motivationDuringAfterChange+engagement+autonomy+positiveNegative+diabetes+firstTime+familyHistoryDiabetes+similarSystem+duration+numberOfSessions+understandingDiabetes,
-         method = 'ML', data = AQ)
-summary(resc)
-
-
-library(lme4)
+library(lmerTest)
 
 n.groups <- 3 # number of groups
 n.repeats <- 26 # samples per group
@@ -15,7 +9,7 @@ groups <- rep(1:n.groups, c(26,26,27))
 typeof(groups)
 groups
 
-res = lmer(finalGoalAchievementWithoutGoalChange ~ milestoneAchievement+age+motivationBeforeDuringChange|groups,
+res = lmer(finalGoalAchievementWithoutGoalChange ~ milestoneAchievement+motivationBeforeDuringChange+milestoneAdherence+condition+agreementPercentage+futureWork+priorEfficacy+postEfficacy+efficacyChange+education+fakeNatural+machineHuman+consciousUnconscious+artificialLifelike+rigidElegant+deadAlive+stagnantLively+mechanicalOrganic+inertInteractive+apatheticResponsive+dislikeLike+unfriendlyFriendly+unkindKind+unpleasantPleasant+awfulNice+incompetentCompetent+ignorantKnowledgeable+irresponsibleResponsible+unintelligentIntelligent+foolishSensible+anxiousRelaxedBefore+agitatedCalmBefore+quiescentSurprisedBefore+anxiousRelaxedAfter+agitatedCalmAfter+quiescentSurprisedAfter+anxiousRelaxedChange+agitatedCalmChange+quiescentSurprisedChange+typeOfRelationship+usefulOrNotDiabetes+usefulOrNotObesity+convenience+preference+motivationBefore+motivation+motivationAfter+motivationBeforeDuringChange+motivationDuringAfterChange+engagement+autonomy+positiveNegative+similarSystem+duration+numberOfSessions+understandingDiabetes+(1+firstTime+gender+familyHistoryDiabetes+age+diabetes|groups),
            REML = TRUE,
            data = AQ)
 summary(res)
